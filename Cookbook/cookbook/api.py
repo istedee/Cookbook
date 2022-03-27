e9fdf664ca07194241605c2e535c43fd107a602c
Mikkeep <mikke.pennanen@gmail.com>
2022-03-27 15:28:51 +0300

    Added ingredients api route

--- Cookbook/cookbook/api.py
+++ Cookbook/cookbook/api.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 
 from .resources.user import UserCollection, UserItem, UserConverter
 from .resources.recipe import RecipeCollection, RecipeItem, RecipeConverter
-
+from .resources.ingredient import IngredientCollection, IngredientConverter, IngredientItem
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
@@ -15,3 +15,5 @@ api.add_resource(UserCollection, "/users/" )
 api.add_resource(UserItem, "/users/<user:user>/")
 api.add_resource(RecipeCollection, "/users/<user:user>/recipes/")
 api.add_resource(RecipeItem, "/users/<user:user>/recipes/<recipe:recipe>/")
+api.add_resource(IngredientCollection, "/ingredients/")
+api.add_resource(IngredientItem, "/ingredients/<ingredient:ingredient>/")
